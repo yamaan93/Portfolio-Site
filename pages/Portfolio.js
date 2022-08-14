238ece48e9cb8c900de1bf60922f191959ff2d6e
Yamaan Bakir <yamaan93@gmail.com>
2022-08-14 13:52:44 -0400

    Add profile site card

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -10,24 +10,25 @@ export default function Portfolio() {
         <SoftwareCard
           title="Support Bot"
           githubLink="https://github.com/yamaan93/Engineering-Support-Bot"
+          image = "../supportbot pfp.png"
         >
-          A discord bot for the unofficial Western Engineering discord. The primary functionally is for providing students with easy
-          to access weekly task lists to help the engineering community keep on
-          track through the riggors of the semester. The tasklist is read from a
+            
+          A discord bot for the unofficial Western Engineering discord. The
+          primary functionally is for providing students with easy to access
+          weekly task lists to help the engineering community keep on track
+          through the riggors of the semester. The tasklist is read from a
           shared Google sheets file so that collaborators can easily update the
           weekly schedule on the fly. This means that no programming or
           technical knowledge is required on behalf of the contributors.
         </SoftwareCard>
         <SoftwareCard
-          title="Support Bot"
+          title="Portfolio Site"
           githubLink="https://github.com/yamaan93/Engineering-Support-Bot"
+          image = "../site.jpg"
         >
-          A discord bot for the unofficial Western Engineering discord. The primary functionally is for providing students with easy
-          to access weekly task lists to help the engineering community keep on
-          track through the riggors of the semester. The tasklist is read from a
-          shared Google sheets file so that collaborators can easily update the
-          weekly schedule on the fly. This means that no programming or
-          technical knowledge is required on behalf of the contributors.
+            
+          With the main goal to learn React and Next.js, this project is the
+          code behind this very website!
         </SoftwareCard>
       </SubjectCard>
     </Layout>
